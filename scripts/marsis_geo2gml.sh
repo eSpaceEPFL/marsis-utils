3953c1fcee8a354f6d6a550ba78fc7dd07333f10
Federico <federico@localhost.localdomain>
2016-09-15 16:33:36 +0200

    Fixed bugs

--- scripts/marsis_geo2gml.sh
+++ scripts/marsis_geo2gml.sh
@@ -3,9 +3,15 @@
 # $2 -> xsl_tranform_model
 # $.. -> file list
 
+
+GEODIR=. #Write here the dir containing the raw geo data
+SNRDIR=. #Write here the dir containing the SNR csv files
+
+GMLDIR=. #Write here the dir to write the gml files to
+
 for f in ${@: 3}
 do
 #   raw_snr2gml.py E${f:1:-7}G.DAT $f E${f:1:-7}G.gml $2 $1
-   marsis_geo2gml.py $f /media/federico/fc_data/MARSIS_data/L2_Data/qi2/R${f:1:-5}snr.csv  /media/federico/Backup/MARSIS/track_gmls_qi/E${f:1:-4}.gml $2 $1
+   marsis_geo2gml.py $GEODIR/$f $SNRDIR/R${f:1:-5}snr.csv  $GMLDIR/E${f:1:-4}.gml $2 $1
 done
 
--- scripts/sharad_geo2gml.sh
+++ scripts/sharad_geo2gml.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-# $1 -> xml_data_model
+# $1 -> xsl_tranform_model
 # $.. -> file list
+
+GEODIR=. #Write here the dir containing the txt geo data
+
+GMLDIR=. #Write here the dir to write the gml files to
 
 for f in ${@: 2}
 do
-    mkdir -p gmls/${f:0:8}
-    sharad_geo2gml.py $f gmls/${f:0:-3}gml $1
+    mkdir -p $GMLDIR/${f:0:8}
+    sharad_geo2gml.py $GEODIR/$f $GMLDIR/${f:0:-3}gml $1
 #    echo $f
 done
 
